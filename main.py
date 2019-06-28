138cff3d69527017f49e0bfa9343cf1be0d3f05b
Artanidos <artanidos@gmail.com>
2019-06-28 19:18:11 +0200

    license added

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+#############################################################################
+# Copyright (C) 2019 Olaf Japp
+#
+# This file is part of Py2Dart.
+#
+#  Py2Dart is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Py2Dart is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Py2Dart. If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
 import sys
 import os
 
